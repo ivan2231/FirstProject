9e3dc8cbbe814a1d85a7e8d25874ca33c9ed1ffc
dgon <fill3432@yandex.ru>
2022-12-08 02:49:24 +0300

    Initial commit

---
+++ app/src/main/java/com/example/firstproject/FirstFragment.kt
@@ -0,0 +1,33 @@
+package com.example.firstproject
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentContainer
+import androidx.navigation.Navigation
+import com.example.firstproject.databinding.FragmentFirstBinding
+import com.example.firstproject.databinding.FragmentSecondBinding
+
+
+class FirstFragment : Fragment() {
+    lateinit var binding: FragmentFirstBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding= FragmentFirstBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.button.setOnClickListener {
+        Navigation.findNavController(binding.root).navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+
+    }
+
